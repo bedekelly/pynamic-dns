7d54757e440aaed6c8630167293df78f6884cd38
bedekelly <bedekelly97@gmail.com>
2014-05-15 21:16:41 +0100

    Initial Commit

---
+++ dnsupdate.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess
+import time
+print("-- -- BEGIN DNS UPDATING -- --")
+
+SECRET_TOKEN = "OHhWMjF5Z3hBWHNYa0h6MktVdVdUbWY5OjExNjMzNjIy"
+
+while True:
+    bcurrent_IP = subprocess.check_output(["curl", "-s", "ifconfig.me"])
+    current_IP = bcurrent_IP.decode(sys.stdout.encoding).strip()
+    out = subprocess.check_output(["curl", "-s", "http://freedns.afraid.org/dynamic/update.php?{}&address={}".format(SECRET_TOKEN, current_IP)])
+    if "ERROR" not in out.decode("utf-8"):
+        subprocess.call(["notify-send", "Updated all DDNS entries."])
+    time.sleep(300)
